3b3eac8e57686fb09249d82971716fa2479b01e4
jeanccruz <76162157+jeanccruz@users.noreply.github.com>
2022-03-02 14:38:48 -0300

    Add files via upload

---
+++ P2-ML_WorkFlow/lambda3.py
@@ -0,0 +1,26 @@
+# low-confidence inferences
+import json
+
+
+THRESHOLD = .7
+
+
+def lambda_handler(event, context):
+
+    # Grab the inferences from the event
+    inferences = event['inferences']
+
+    # Check if any values in our inferences are above THRESHOLD
+    meets_threshold =  any(x > THRESHOLD for x in inferences)
+
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    if meets_threshold:
+        pass
+    else:
+        raise ValueError("THRESHOLD_CONFIDENCE_NOT_MET")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
